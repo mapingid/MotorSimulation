8d5693407da85f1b5c91a94c9efd0b4ea537c2ac
Firman Ardiyansyah <firman.ardiyansyah@formulatrix.com>
2023-02-01 11:11:29 +0700

    Initial Commit;

---
+++ MotorSimulation/Motor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorSimulation
+{
+  class Motor
+  {
+    int Position;
+    void Move(int targetPosition )
+    {
+
+    }
+  }
+}
